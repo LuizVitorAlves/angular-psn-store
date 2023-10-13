3af450d8b3e3b7f61a57a023222615e67f590371
Luiz Vitor Alves <l.v.alvesjr@gmail.com>
2023-10-12 23:25:29 -0300

    first commit

---
+++ src/app/components/cards/cards.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cards',
+  templateUrl: './cards.component.html',
+  styleUrls: ['./cards.component.css']
+})
+export class CardsComponent implements OnInit {
+
+  @Input()
+  gameCover:string = ""
+  @Input()
+  gameLabel:string= ""
+  @Input()
+  gameType:string = ""
+  @Input()
+  gamePrice: string = ""
+  @Input()
+  gameTitle:string = ""
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
